a19c344a3248a0b5d498e1df5525846dd5785ef6
Shaurya Jha <shauryajha35@gmail.com>
2025-03-10 22:42:44 +0530

    [Add Web Dashboard]

--- apps/tests/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: "tests",
-  testEnvironment: "node",
-  roots: ["<rootDir>/__tests__"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  moduleFileExtensions: ["ts", "tsx", "js"],
-};
